f109c0e0711d40fa338fb3521a1d5b698ca3a640
g.kaba <gorgekaba@gmail.com>
2020-11-27 15:02:50 -0500

    first commit

---
+++ src/main/java/com/mycompany/mavenjenkins/control/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenjenkins.control;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ *
+ * @author georg
+ */
+public class Main {
+    
+        public static void main(String[] args) throws IOException, ParseException {
+       
+            System.out.println("hello world");
+    }
+}
